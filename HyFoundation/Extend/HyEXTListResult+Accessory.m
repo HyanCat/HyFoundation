5f0e5f06634d4cbd63c561e853eebf741a583834
HyanCat <hyancat@live.cn>
2016-04-01 18:21:59 +0800

    update

--- HyFoundation/Extend/HyEXTListResult+Accessory.m
+++ HyFoundation/Extend/HyEXTListResult+Accessory.m
@@ -19,23 +19,4 @@
 	self.list = models;
 }
 
-- (NSUInteger)minId
-{
-	// 一般是已排序
-	return [[self.list.lastObject id] unsignedIntegerValue];
-}
-
-- (NSUInteger)minUpdateTime
-{
-	__block NSTimeInterval minTime = [NSDate date].timeIntervalSince1970;
-	[self.list enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-		if ([obj respondsToSelector:@selector(updateTime)]) {
-			if ([obj updateTime].timeIntervalSince1970 < minTime) {
-				minTime = [obj updateTime].timeIntervalSince1970;
-			}
-		}
-	}];
-	return minTime;
-}
-
 @end
